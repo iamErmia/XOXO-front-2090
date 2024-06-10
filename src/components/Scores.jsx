3b6772789e731905413d5c7954948f2078bad6fd
iamErmia <iam.ermia@yahoo.com>
2024-06-10 21:12:30 +0330

    Finished the Score table page

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,7 +1,25 @@
-const Scores = () => {
-    return (
-        <div>Scores Table</div>
-    );
-};
-
-export default Scores;
+export default function Scores() {
+  return (
+    <>
+      <h1 className="headTxt">Tic-Tac-Toe</h1>
+      <h1 className="headTxt">SCORE TABLE (TOP-10)</h1>
+      <table>
+        <tr>
+            <th>ROW</th>
+            <th>NAME</th>
+            <th>SCORE</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Ermia</td>
+            <td>8</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Beni</td>
+            <td>6</td>
+        </tr>
+      </table>
+    </>
+  );
+}
